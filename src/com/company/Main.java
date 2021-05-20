4e977e8b2268c7dc0ec7bc3e9a57197c7ea81c93
Zhanel-ship-it <78585664+Zhanel-ship-it@users.noreply.github.com>
2021-05-20 23:33:00 +0600

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,76 @@
+package com.company;
+
+public class Main {
+    static void mergesort(int arr[]) {
+        int n = arr.length;
+        if (n < 2)
+            return;
+        int mid = n / 2;
+        int left[] = new int[mid];
+        int right[] = new int[n - mid];
+        for (int i = 0; i < mid; i++)
+            left[i] = arr[i];
+        for (int i = mid; i < n; i++)
+            right[i - mid] = arr[i];
+        mergesort(left);
+        mergesort(right);
+        merge(arr, left, right);
+    }
+
+    public static void merge(int arr[], int left[], int right[]) {
+        int nL = left.length;
+        int nR = right.length;
+        int i = 0, j = 0, k = 0;
+        while (i < nL && j < nR) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < nL) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < nR) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void quicksort(int arr[], int start, int end) {
+        if (start < end) {
+            int pIndex = partition(arr, start, end);
+            quicksort(arr, start, pIndex - 1);
+            quicksort(arr, pIndex + 1, end);
+        }
+    }
+
+    static int partition(int arr[], int start, int end) {
+        int pivot = arr[end];
+        int pIndex = start;
+        for (int i = start; i < end; i++) {
+            if (arr[i] <= pivot) {
+                swap(arr, i, pIndex);
+                pIndex++;
+            }
+        }
+        swap(arr, pIndex, end);
+        return pIndex;
+    }
+
+    static void swap(int arr[], int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
